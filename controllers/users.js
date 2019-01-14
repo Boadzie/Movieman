55c725a5cde0725378ac9bf33c1545c7d7846699
Boadzie Daniel <boadziedaniel43@gmail.com>
2019-01-14 11:10:01 +0000

    Added login model, route and view

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,11 @@
 const User = require('../models/User.js');
+const passportJWT = require('passport-jwt');
+const jwt = require('jsonwebtoken');
+
+const ExtractJwt = passportJWT.ExtractJwt;
+const jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+jwtOptions.secretOrKey = 'thisisthesecretkey';
 
 module.exports.controller = (app) => {
   // register a user
@@ -21,5 +28,39 @@ module.exports.controller = (app) => {
         user,
       });
     });
+  });
+
+
+  // login a user
+  app.post('/users/login', (req, res) => {
+    if (req.body.email && req.body.password) {
+      const email = req.body.email;
+      const password = req.body.password;
+      User.getUserByEmail(email, (err, user) => {
+        if (!user) {
+          res.status(404).json({
+            message: 'The user does not exist!',
+          });
+        } else {
+          User.comparePassword(password, user.password, (error, isMatch) => {
+            if (error) throw error;
+            if (isMatch) {
+              const payload = {
+                id: user.id,
+              };
+              const token = jwt.sign(payload, jwtOptions.secretOrKey);
+              res.json({
+                message: 'ok',
+                token,
+              });
+            } else {
+              res.status(401).json({
+                message: 'The password is incorrect!',
+              });
+            }
+          });
+        }
+      });
+    }
   });
 };
---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-layout justify-center align-item-center>
+      <v-flex md6>
+        <v-card color="grey lighten-4 white-text p-5">
+          <v-form v-model="valid" ref="form" lazy-validation>
+            <v-text-field label="Email" v-model="email" :rules="emailRules" required></v-text-field>
+            <v-text-field label="Password" v-model="password" required></v-text-field>
+            <v-btn @click="submit" :disabled="!valid">submit</v-btn>
+            <v-btn @click="clear">clear</v-btn>
+          </v-form>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
